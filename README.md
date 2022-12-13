a26d0e7880043a1e17e5362ac0267aaea20aa8f5
Adam T. Gautier <adam@gautier.org>
2022-12-13 08:01:33 -0500

    Nothing put improvement.

--- README.md
+++ README.md
@@ -1,21 +1,3 @@
-# spicyuno
+# About spicyuno
 
-This is the repository for all things spicy uno. 
-
-## Legal Stuff
-
-This is **NOT** [Mattel's](https://www.mattel.com) card game [*UNO*](https://www.amazon.com/Mattel-Games-UNO-Retro-Editon/dp/B07DDJNHFF/ref=sr_1_4?crid=X3NNAOJV5JAY&keywords=uno+classic&qid=1666374231&qu=eyJxc2MiOiI0LjA1IiwicXNhIjoiMy41NyIsInFzcCI6IjMuNTEifQ%3D%3D&sprefix=uno+classic%2Caps%2C123&sr=8-4), although you need a deck to play.
-
-## Website
-
-[spicyuno.fun](http://www.spicyuno.fun) hosts the content of this site for public consumption.
-
-## Rules
-
-The rules of spicyuno are documented in [Base Spicy Uno Rules](...). These rules should be thought of as the starting point for your own spicyuno. The [base rules](...) may change over time and should be considered as the bottom line.  In the event of a rule conflict or dispute then the latest [base rules](...) should take prescedent over customized rules.
-
-### How do I propose a new rule?
-
-Well... currently there is not a way, it is totally possible and that is the goal of the [spicyuno.fun](http://www.spicyuno.fun) project.  Please stay tuned for more information.
-
-https://www.unorules.com/uno-versions/
+**spicyuno** is our family's enhancement of [Mattel's](https://www.mattel.com) [*UNO*](https://www.amazon.com/Mattel-Games-UNO-Retro-Editon/dp/B07DDJNHFF/ref=sr_1_4?crid=X3NNAOJV5JAY&keywords=uno+classic&qid=1666374231&qu=eyJxc2MiOiI0LjA1IiwicXNhIjoiMy41NyIsInFzcCI6IjMuNTEifQ%3D%3D&sprefix=uno+classic%2Caps%2C123&sr=8-4) card game. The idea is to add rules to each card that are applied when the card is played. There are a few non-card based rules that apply during the game which just increses the spice level.  We hope you have fun and enjoy this fun [variant](https://www.unorules.com/uno-versions/) of **UNO**.
--- main.js
+++ main.js
@@ -1,43 +1,49 @@
 import {micromark} from 'https://esm.sh/micromark@3?bundle';
-
-function closeHamburger() {
-    var control = document.querySelector('#hamburger');
-    control.checked=false;
-}
-
-function renderContent(uri) {
-  var elements = document.getElementsByClassName('dynamic-content');
-  for(var i = 0; i < elements.length; i++) {
-     var eid = elements[i].id
-     loadContent(eid, uri + eid);
-     // console.log(elements[i].id);
-  }
-}
     
-function loadContent(eid, uri) {
-   // read text from URL location
-   var request = new XMLHttpRequest();
-   request.open('GET', uri, true);
-   request.send(null);
-   request.onreadystatechange = function () {
-      if (request.readyState === 4 && request.status === 200) {
-         var type = request.getResponseHeader('Content-Type');
-         if (type.indexOf("text") !== 1) {
-            var content =  request.responseText;
-            var element = document.getElementById(eid);
-            // var rendered = md.render(content);
-            var rendered = micromark(content);
-            element.innerHTML = rendered;
-            // For debugging
-            // console.log(element.innerHTML);
-            // console.log(rendered);
-            // console.log(content);
-            // console.log(eid);
-   
-         }
-      }
-   }
-   
-}
-
-renderContent("https://raw.githubusercontent.com/gautada/spicyuno.fun/main/");
+    function loadContent(uri) {
+        var elements = document.getElementsByClassName('dynamic-content');
+        for(var i = 0; i < elements.length; i++) {
+            var eid = elements[i].id
+            if (eid.endsWith(".md")) {
+                fetchContent(eid, uri + eid, true);
+            } else if (eid.endsWith(".html")) {
+                fetchContent(eid, uri + eid, false);
+            }
+        }
+    }
+    
+    function fetchContent(eid, uri, render) {
+        var request = new XMLHttpRequest();
+        request.open('GET', uri, true);
+        request.send(null);
+        request.onreadystatechange = function () {
+            if (request.readyState === 4 && request.status === 200) {
+                var type = request.getResponseHeader('Content-Type');
+                if (type.indexOf("text") !== 1) {
+                    var content =  request.responseText;
+                    var element = document.getElementById(eid);
+                    var rendered = content;
+                    if (render) {
+                        rendered = micromark(content);
+                    }
+                    element.innerHTML = rendered;
+                }
+            }
+        }
+        
+    }
+    
+    loadContent("https://raw.githubusercontent.com/gautada/spicyuno.fun/main/");
+    // loadContent("http://www.spicyuno.fun/");
+    
+    function closeHamburger() {
+        var control = document.querySelector('#hamburger');
+        control.checked=false;
+    }
+    
+    window.onload = function(){
+        var elements = document.querySelectorAll('.hamburger-close');
+        elements.forEach(function(value){
+          value.onclick = function() { closeHamburger(); };
+        });
+    };
